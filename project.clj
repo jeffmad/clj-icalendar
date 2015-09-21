c81b7151217ad9b3ec74f4215796c566d4e7e254
Jeff Madynski <jmadynski@expedia.com>
2015-09-20 22:36:10 -0700

    Initial Checkin

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject org.clojars.jeffmad.clj-icalendar "0.1.0-SNAPSHOT"
+  :description "wrapper over ical4j to generate ics"
+  :url "http://github.com/jeffmad/clj-icalendar"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.mnode.ical4j/ical4j "1.0.6"]
+  :global-vars {*warn-on-reflection* true
+                *assert* false})
