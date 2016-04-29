2668cf208478d63936913c605a7a7c49e8c721b2
Roberto Robles <robertorobles0@gmail.com>
2016-04-29 08:26:48 -0700

    Change project name so we can push it to clojars

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-icalendar/clj-icalendar "0.1.4"
+(defproject org.clojars.vitallabs/clj-icalendar "0.1.4"
   :description "wrapper over ical4j to generate ics"
-  :url "http://github.com/jeffmad/clj-icalendar"
+  :url "http://github.com/roberto-robles/clj-icalendar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
-        :url "https://github.com/jeffmad/clj-icalendar"}
+        :url "https://github.com/roberto-robles/clj-icalendar"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.mnode.ical4j/ical4j "1.0.6"]]
   :global-vars {*warn-on-reflection* false
